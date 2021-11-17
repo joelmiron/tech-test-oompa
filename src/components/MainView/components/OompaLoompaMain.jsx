9fefed69e15cfedbb5e3edf33c8a5f5beac3b5d5
Joel Mirón <joelmironpub@gmail.com>
2021-11-17 23:29:20 +0100

    intersection

--- src/components/MainView/components/OompaLoompaMain.jsx
+++ src/components/MainView/components/OompaLoompaMain.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useRef,useState } from "react";
 
 
 
 const OompaLoompaMain = ({ id, image, firstName, lastName,gender,profession}) =>{
+const [showImage,setShowImage] = useState(false)
+const elementRef = useRef()
+
+
+useEffect(() =>{
+const viewChange = (entries) =>{
+   const image = entries[0]
+   if(image.isIntersecting){
+     // setShowImage(true)
+     console.log(image.isIntersecting)
+   }
+}
+
+
+
+const observer = new IntersectionObserver(viewChange, {  rootMargin: '100px'});
+observer.observe(elementRef.current)
+
+},[])
 
 return(
-<div className="oompaLoompaContent">
+
+ <div className="oompaLoompaContent" ref={elementRef} >
    <img alt="OompaImage" src={image} /> 
    <div>
 <div className="name">{firstName} {lastName}</div>
