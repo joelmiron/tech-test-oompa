256731d4e6b7ae99d99a3757ba985cbe3cdfa951
Joel Mirón <joelmironpub@gmail.com>
2021-11-20 18:54:56 +0100

    detailPage

--- src/components/MainView/components/OompaLoompaDetail.jsx
+++ src/components/MainView/components/OompaLoompaDetail.jsx
@@ -8,16 +8,30 @@ export const OompaLoompaDetail = () =>{
     const type = "single"
     let {id} = useParams();
     const api= "https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas/"
-    const [oompas, setOompas,oompasToFilter] = useGetOompas(id,type,api);
+    const [oompa] = useGetOompas(id,type,api);
 
 
     useEffect(() =>{
-        window.localStorage.setItem(type+"storagedOompaLoompas", JSON.stringify(oompas));
-      },[oompas])
+        window.localStorage.setItem(type+"storagedOompaLoompas", JSON.stringify(oompa));
+      },[oompa])
       
-return(<div>
+return(<div className="detail">
 
-{id}
+<div className="detailContainer">
+<div className="oompaImage"><img src={oompa.image} alt="OompaImage"/></div>
+<div className="oompaDetail">
+    <div  className="infoSpace"> 
+<div className="name">{oompa.first_name} {oompa.last_name}</div>
+<div className="gender">{oompa.gender === "F" ? "Female" : "Man"}</div>
+<div className="profession">{oompa.profession}</div>
+</div>
+
+{oompa.description}<br/>
+
+
+</div>
+
+</div>
 
 
 </div>)
