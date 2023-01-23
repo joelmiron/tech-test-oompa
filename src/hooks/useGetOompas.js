4f26b2f8c88186c18970499795a6306c101b8ffe
Joel Mirón <joelmironpub@gmail.com>
2023-01-23 22:40:20 +0100

    scrollstep1

--- src/hooks/useGetOompas.js
+++ src/hooks/useGetOompas.js
@@ -11,19 +11,43 @@ export const useGetOompas = (id, type,api) => {
   const refreshingDateStorage = JSON.parse(window.localStorage.getItem(type+"clearDate"));
   const [oompas, setOompas] = useState(type === "all" ? JSON.parse(window.localStorage.getItem("allstoragedOompaLoompas")) || []
                                       : JSON.parse(window.localStorage.getItem(id+"storagedOompaLoompa")) || []);
-  const [oompasToFilter, setOompasToFilter] = useState([]);
-  var actualDate = moment().format("LLL");
-  var clearDate = moment().add(1, "day").format("LLL");
- 
+  const [oompasToFilter, setOompasToFilter] = useState(JSON.parse(window.localStorage.getItem("allstoragedOompaLoompas")) || []);
+  let actualPage = JSON.parse(window.localStorage.getItem("actualPage")) ?  JSON.parse(window.localStorage.getItem("actualPage")) : 1;
+  let actualDate = moment().format("LLL");
+  let clearDate = moment().add(1, "day").format("LLL");
+ const [idPage, setIdPage] = useState(JSON.parse(window.localStorage.getItem("actualPage")) ?  JSON.parse(window.localStorage.getItem("actualPage")) + 1 : 1)
   useEffect(() => {
     getOompas();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [id]);
  
     const getOompas = async () => {
     window.localStorage.setItem(type+"actualDate", JSON.stringify(actualDate));
-  
-  
+
+if(idPage > actualPage && type ==="all" ) {
+  window.localStorage.setItem("actualPage", JSON.stringify(idPage));
+  setIdPage((idPage) => idPage + 1)
+  const response = await axios.get(customApi);
+  setOompas((prevOompas) => prevOompas.concat(response.data.results) );
+  setOompasToFilter((oompas) => oompas.concat(response.data.results));
+  window.localStorage.setItem(type+"actualDate", JSON.stringify(actualDate));
+   window.localStorage.setItem(type+"clearDate", JSON.stringify(clearDate)); 
+  const storagedOompas = JSON.parse(window.localStorage.getItem("allstoragedOompaLoompas")); 
+   const mergedOompas =  storagedOompas.concat(response.data.results)
+   window.localStorage.setItem("allstoragedOompaLoompas", JSON.stringify(mergedOompas)); 
+ 
+} 
+
+if(moment(actualDateStorage).isAfter(refreshingDateStorage)){
+  localStorage.removeItem(type+"clearDate");
+  localStorage.removeItem(type+"actualdate");
+  localStorage.removeItem(id+"storagedOompaLoompa");
+  localStorage.removeItem("actualPage");
+  setIdPage(1)
+  setOompas([]);
+  setOompasToFilter([]);
+}
+
 
     //data can exists but refresh time is up, then need to clear storage or if data is empty, then fill oompas with the api fetch
     if (oompas.length === 0 || moment(actualDateStorage).isAfter(refreshingDateStorage)) {
@@ -41,6 +65,7 @@ export const useGetOompas = (id, type,api) => {
           setOompas(response.data.results);
           setOompasToFilter(response.data.results);
           window.localStorage.setItem(type + "storagedOompaLoompas", JSON.stringify(response.data.results));
+          setIdPage((actualId) => actualId + 1)
         }else{
           setOompas(response.data);
         }
@@ -49,6 +74,8 @@ export const useGetOompas = (id, type,api) => {
         window.localStorage.setItem(type+"actualDate", JSON.stringify(actualDate));
         window.localStorage.setItem(type+"clearDate", JSON.stringify(clearDate));  
     }
+
+ 
 }
  
 
